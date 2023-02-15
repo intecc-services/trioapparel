a2d480e6c5cdb3a842a26cbd789287740e33b8b4
Adithya S K <adithyaskolavi@gmail.com>
2023-02-15 15:48:38 +0530

    added infrastructure to navigation bar in mobile view - Adithya S K

--- frontend/src/components/navbar/NavbarMobile.js
+++ frontend/src/components/navbar/NavbarMobile.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react'
-import {FaBars} from 'react-icons/fa'
-import {CgClose} from 'react-icons/cg'
-import './styles.css'
-import { navbarContents } from '../../general'
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { CgClose } from 'react-icons/cg';
+import './styles.css';
+import { navbarContents } from '../../general';
+import { NavLink } from 'react-router-dom';
 
 const NavbarMobile = () => {
-    const [click,setClick] = useState(false)
+  const [click, setClick] = useState(false);
   return (
-    <div className='navbar-mobile-main'>
-        {
-        click?<div className='nav-icon-wrapper'><CgClose color='white' size={'1.5rem'} className='bars' onClick={()=>setClick(!click)}/></div>:<div className='nav-icon-wrapper'><FaBars color='white' size={'1.5rem'} className='bars' onClick={()=>setClick(!click)}/></div>
-        }{
-            click?<div className='drop-down'>
-                {
-                    navbarContents.map((item)=>{
-                        return <div className='nav-mob-item'>
-                            {item}
-                        </div>
-                    })
-                }
-            </div>:<></>
-        }
+    <div className="navbar-mobile-main">
+      {click ? (
+        <div className="nav-icon-wrapper">
+          <CgClose
+            color="white"
+            size={'1.5rem'}
+            className="bars"
+            onClick={() => setClick(!click)}
+          />
+        </div>
+      ) : (
+        <div className="nav-icon-wrapper">
+          <FaBars
+            color="white"
+            size={'1.5rem'}
+            className="bars"
+            onClick={() => setClick(!click)}
+          />
+        </div>
+      )}
+      {click ? (
+        <div className="drop-down">
+          {navbarContents.map((item) => {
+            return (
+              <div className="nav-mob-item">
+                <NavLink className="nav-link" to={item}>
+                  {item}
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default NavbarMobile
+export default NavbarMobile;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import { HashRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Infrastructure from './pages/Infrastructure';
+// import Layout from './components/Layout';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Router> */}
-      <HashRouter>
-        <App />
-      </HashRouter>
-    {/* </Router> */}
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="*" element={<App />} />
+        <Route path="/Infrastructure" element={<Infrastructure />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
